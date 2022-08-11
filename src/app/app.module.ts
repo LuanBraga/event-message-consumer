e068348514f746dacd84143d7aabda38631cea9e
LuanBraga <luanbraga.lb@gmail.com>
2022-08-11 18:49:29 -0300

    init remote repository

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotificationIndicatorComponent } from './components/notification-indicator/notification-indicator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotificationIndicatorComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/notification-indicator/notification-indicator.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+
+interface Message {
+  id: number;
+  description: string;
+  url: string;
+}
+
+@Component({
+  selector: 'app-notification-indicator',
+  templateUrl: './notification-indicator.component.html',
+  styleUrls: ['./notification-indicator.component.scss']
+})
+export class NotificationIndicatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    const eventSource = new EventSource('http://localhost:8080/api/v1/messages/sse-emitter');
+    
+    const eventIdList = document.querySelector('ul');
+
+    // const eventSource = new EventSource('http://localhost:8080/api/v1/events/sse-emitter');
+
+
+    // eventSource.onmessage = (e) => {
+    //   console.log(e.data);
+    // }
+
+    eventSource.addEventListener('message', (e) => {
+      let m = e.data as Message;
+
+      const newElement = document.createElement("li");
+
+      newElement.textContent = `${m}`;
+
+      eventIdList?.appendChild(newElement);
+
+      console.log(m);
+    });
+
+    eventSource.addEventListener('open', (e) => {
+      console.log(e);
+    });
+  }
+
+}
